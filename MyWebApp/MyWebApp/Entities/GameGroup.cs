4c0b02d8a1058df0f317a5d559b10667e601146e
AVPa1ly <andrewvorobiov@gmail.com>
2021-11-05 11:43:42 +0300

    Complete part of Lab5 tasks

---
+++ MyWebApp/MyWebApp/Entities/GameGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApp.Entities
+{
+    public class GameGroup
+    {
+        public int GameGroupId { get; set; }
+        public string GroupName { get; set; }
+
+        /// <summary>
+        /// Навигационное свойство один-ко-многим
+        /// </summary>
+        public List<Game> Games { get; set; }
+    }
+}
