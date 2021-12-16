0c3ba7378c5efade603186c1ae14fc9845046e75
AVPa1ly <andrewvorobiov@gmail.com>
2021-12-16 21:31:33 +0300

    Complete Lab6

--- MyWebApp/MyWebApp/Data/DbInitializer.cs
+++ MyWebApp/MyWebApp/Data/DbInitializer.cs
@@ -49,6 +49,68 @@ namespace MyWebApp.Data
                 admin = await userManager.FindByNameAsync("admin@mail.ru");
                 await userManager.AddToRoleAsync(admin, "admin");
             }
+
+            // Проверка наличия групп объектов
+            if (!context.GameGroups.Any())
+            {
+                context.GameGroups.AddRange(
+                    new List<GameGroup>
+                    {
+                        new GameGroup{GroupName = "Шутеры"},
+                        new GameGroup{GroupName = "Спортивные"},
+                        new GameGroup{GroupName = "Файтинги"},
+                        new GameGroup{GroupName = "Экшен-РПГ"},
+                        new GameGroup{GroupName = "Приключения"},
+                        new GameGroup{GroupName = "Прочие"},
+                    });
+                await context.SaveChangesAsync();
+            }
+
+            // Проверка наличия объектов
+            if (!context.Games.Any())
+            {
+                context.Games.AddRange(
+                    new List<Game>
+                    {
+                        new Game{GameName = "FIFA 22", Description = "Задонать. Быстро. А то получишь по попе",
+                            Cost = 5499, GameGroupId = 2, Image = "fifa.jpg"},
+                        new Game{GameName = "Call of Duty: Modern Warfare (2019)", Description = "Русские проводят геноцид коренных урзыкстанцев",
+                            Cost = 4999, GameGroupId = 1, Image = "codmw.jpg"},
+                        new Game{GameName = "Cyberpunk 2077", Description = "Чапи-баги слайдшоу с Киану Ривзом в главной роли",
+                            Cost = 3219, GameGroupId = 4, Image = "cyberpunk.jpg"},
+                        new Game{GameName = "Detroit: Become Human", Description = "Мыльная опера про любовь, смерть и роботов",
+                            Cost = 2149, GameGroupId = 5, Image = "detroit.jpg"},
+                        new Game{GameName = "Grand Theft Auto 5", Description = "Три дурачка с пушками ищут НЛО в Лос-Сантосе",
+                            Cost = 2849, GameGroupId = 1, Image = "gta.jpg"},
+                        new Game{GameName = "Hades", Description = "Пошел как-то Петрович на прогулку в Ад...",
+                            Cost = 1789, GameGroupId = 4, Image = "hades.jpg"},
+                        new Game{GameName = "Hot Wheels Unleashed", Description = "Мало было пластмассок, теперь еще и на приставке",
+                            Cost = 3219, GameGroupId = 6, Image = "hot-wheels.jpg"},
+                        new Game{GameName = "Just Dance 2022", Description = "Танцуй, пока не кончится дождь",
+                            Cost = 3999, GameGroupId = 6, Image = "just-dance.jpg"},
+                        new Game{GameName = "Mortal Kombat X", Description = "Побей своего друга, рандомно протыкивая все кнопки геймпада",
+                            Cost = 1429, GameGroupId = 3, Image = "mortal-kombat.jpg"},
+                        new Game{GameName = "NBA 2k22", Description = "Это как FIFA, но про баскетбол",
+                            Cost = 5499, GameGroupId = 2, Image = "nba.jpg"},
+                        new Game{GameName = "NHL 22", Description = "Хоккейчик, мужчины походят на Овечкина",
+                            Cost = 5499, GameGroupId = 2, Image = "nhl.jpg"},
+                        new Game{GameName = "Rayman Legends", Description = "Два веселых гуся",
+                            Cost = 1429, GameGroupId = 5, Image = "rayman.jpg"},
+                        new Game{GameName = "Marvel's Spider Man", Description = "Does whatever a spider can",
+                            Cost = 2849, GameGroupId = 4, Image = "spider-man.jpg"},
+                        new Game{GameName = "Marvel's Spider Man: Miles Morales", Description = "Does whatever a black man can",
+                            Cost = 4299, GameGroupId = 4, Image = "spider-man-miles.jpg"},
+                        new Game{GameName = "Ghost of Tsushima", Description = "Злой монгола украла наш рис",
+                            Cost = 4999, GameGroupId = 4, Image = "tsushima.jpg"},
+                        new Game{GameName = "Assasin's Creed Valhalla", Description = "РПГ, притворяющееся Assasin's Creed-ом",
+                            Cost = 4499, GameGroupId = 4, Image = "valhalla.jpg"},
+                        new Game{GameName = "Watch Dogs: Legion", Description = "Обо всех и ни о чем",
+                            Cost = 4499, GameGroupId = 4, Image = "watch-dogs.jpg"},
+                        new Game{GameName = "The Witcher 3: Wild Hunt", Description = "Ламберт, Ламберт, хер моржовый, Ламберт, Ламберт, вредный...",
+                            Cost = 2599, GameGroupId = 4, Image = "witcher.jpg"}
+                    });
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
---
+++ MyWebApp/MyWebApp/Migrations/20211216181419_EntitiesAdded.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyWebApp.Migrations
+{
+    public partial class EntitiesAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameGroups",
+                columns: table => new
+                {
+                    GameGroupId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GroupName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameGroups", x => x.GameGroupId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GameName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cost = table.Column<int>(type: "int", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GameGroupId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.GameId);
+                    table.ForeignKey(
+                        name: "FK_Games_GameGroups_GameGroupId",
+                        column: x => x.GameGroupId,
+                        principalTable: "GameGroups",
+                        principalColumn: "GameGroupId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_GameGroupId",
+                table: "Games",
+                column: "GameGroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "GameGroups");
+        }
+    }
+}
