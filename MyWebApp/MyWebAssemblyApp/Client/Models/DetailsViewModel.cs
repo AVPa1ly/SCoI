59f2df880c989d081567124f149e45e2237be4d2
AVPa1ly <andrewvorobiov@gmail.com>
2021-12-22 11:00:16 +0300

    Complete Lab8

---
+++ MyWebApp/MyWebAssemblyApp/Client/Models/DetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MyWebAssemblyApp.Client.Models
+{
+    public class DetailsViewModel
+    {
+        [JsonPropertyName("gameName")]
+        public string GameName { get; set; } // Название игры
+        [JsonPropertyName("description")]
+        public string Description { get; set; } // Описание игры
+        [JsonPropertyName("cost")]
+        public int Cost { get; set; } // Стоимость экземпляра игры
+        [JsonPropertyName("image")]
+        public string Image { get; set; } // Имя файла изображения
+    }
+}
---
+++ MyWebApp/MyWebAssemblyApp/Client/Models/ListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MyWebAssemblyApp.Client.Models
+{
+    public class ListViewModel
+    {
+        [JsonPropertyName("gameId")]
+        public int GameId { get; set; } // ID игры
+        [JsonPropertyName("gameName")]
+        public string GameName { get; set; } // Название игры
+    }
+}
